f512278ed83675565b096563619654b6dc87ca68
jjaoguedes <joaoguedes1800@gmail.com>
2024-01-18 21:10:48 -0300

    Uma ALU de 1 bit que realiza AND, OR e adição entre a e b ou a– e b–

--- dsf_ALU/simulation/qsim/work/dsf_@a@l@u/_primary.vhd
+++ dsf_ALU/simulation/qsim/work/dsf_@a@l@u/_primary.vhd
@@ -4,10 +4,11 @@ entity dsf_ALU is
     port(
         Result          : out    vl_logic;
         a               : in     vl_logic;
+        Ainvert         : in     vl_logic_vector(1 downto 0);
         b               : in     vl_logic;
         Binvert         : in     vl_logic_vector(1 downto 0);
         CarryIn         : in     vl_logic;
-        Operacao        : in     vl_logic_vector(1 downto 0);
+        operacao        : in     vl_logic_vector(1 downto 0);
         CarryOut        : out    vl_logic
     );
 end dsf_ALU;
--- dsf_ALU/simulation/qsim/work/dsf_@a@l@u_vlg_sample_tst/_primary.vhd
+++ dsf_ALU/simulation/qsim/work/dsf_@a@l@u_vlg_sample_tst/_primary.vhd
@@ -3,10 +3,11 @@ use verilog.vl_types.all;
 entity dsf_ALU_vlg_sample_tst is
     port(
         a               : in     vl_logic;
+        Ainvert         : in     vl_logic_vector(1 downto 0);
         b               : in     vl_logic;
         Binvert         : in     vl_logic_vector(1 downto 0);
         CarryIn         : in     vl_logic;
-        Operacao        : in     vl_logic_vector(1 downto 0);
+        operacao        : in     vl_logic_vector(1 downto 0);
         sampler_tx      : out    vl_logic
     );
 end dsf_ALU_vlg_sample_tst;
