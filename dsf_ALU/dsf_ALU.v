c0db498f54fc3d2a51428f0b4391c1a09cce0789
jjaoguedes <joaoguedes1800@gmail.com>
2024-01-20 17:42:09 -0300

    Revert "ALU de 1 bit com ajuste de Less e da junção do Binvert com CarryIn(Bnegate)"
    This reverts commit 387c08f3f4278678cb850f659601a35be560dfc7.

--- dsf_ALU/dsf_ALU.v
+++ dsf_ALU/dsf_ALU.v
@@ -14,42 +14,34 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition"
-// CREATED		"Sat Jan 20 17:07:17 2024"
+// CREATED		"Wed Jan 17 17:37:29 2024"
 
-module dsf_ALU(
+module dsf_ALU #(parameter WIDTH = 1)(
 	a,
-	CarryIn,
 	b,
-	Ainvert,
-	Binvert,
+	CarryIn,
 	operacao,
 	Result,
 	CarryOut
 );
 
 
-input wire	a;
+input wire	[WIDTH-1:0] a;
+input wire	[WIDTH-1:0] b;
 input wire	CarryIn;
-input wire	b;
-input wire	[1:0] Ainvert;
-input wire	[1:0] Binvert;
 input wire	[1:0] operacao;
 output wire	Result;
 output wire	CarryOut;
 
-wire	SYNTHESIZED_WIRE_0;
-wire	SYNTHESIZED_WIRE_1;
-wire	SYNTHESIZED_WIRE_2;
-wire	SYNTHESIZED_WIRE_11;
-wire	SYNTHESIZED_WIRE_12;
-wire	SYNTHESIZED_WIRE_9;
-wire	SYNTHESIZED_WIRE_10;
+wire [WIDTH-1:0]	SYNTHESIZED_WIRE_0;
+wire [WIDTH-1:0]	SYNTHESIZED_WIRE_1;
+wire [WIDTH-1:0]	SYNTHESIZED_WIRE_2;
 
 
 
 
 
-dsf_Mult3to1	b2v_inst(
+dsf_Mult3to1 #(.WIDTH(WIDTH))	b2v_inst(
 	.In0(SYNTHESIZED_WIRE_0),
 	.In1(SYNTHESIZED_WIRE_1),
 	.In2(SYNTHESIZED_WIRE_2),
@@ -57,34 +49,16 @@ dsf_Mult3to1	b2v_inst(
 	.Out(Result));
 
 
-dsf_somador_completo	b2v_inst1(
-	.a(SYNTHESIZED_WIRE_11),
-	.b(SYNTHESIZED_WIRE_12),
+dsf_somador_completo #(.WIDTH(WIDTH))	b2v_inst1(
+	.a(a),
+	.b(b),
 	.CarryIn(CarryIn),
 	.soma(SYNTHESIZED_WIRE_2),
 	.CarryOut(CarryOut));
 
-assign	SYNTHESIZED_WIRE_0 = SYNTHESIZED_WIRE_11 & SYNTHESIZED_WIRE_12;
+assign	SYNTHESIZED_WIRE_0 = a & b;
 
-assign	SYNTHESIZED_WIRE_1 = SYNTHESIZED_WIRE_12 | SYNTHESIZED_WIRE_11;
-
-
-dsf_Mult2to1	b2v_inst2(
-	.In0(b),
-	.In1(SYNTHESIZED_WIRE_9),
-	.Sel(Binvert),
-	.Out(SYNTHESIZED_WIRE_12));
-
-assign	SYNTHESIZED_WIRE_9 =  ~b;
-
-
-dsf_Mult2to1	b2v_inst4(
-	.In0(a),
-	.In1(SYNTHESIZED_WIRE_10),
-	.Sel(Ainvert),
-	.Out(SYNTHESIZED_WIRE_11));
-
-assign	SYNTHESIZED_WIRE_10 =  ~a;
+assign	SYNTHESIZED_WIRE_1 = b | a;
 
 
 endmodule
